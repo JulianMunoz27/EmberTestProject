d72132a74017fcfd8237f204c62d2b8c455ac1b7
Julian Munoz Bedoya <jmunoz@spear-tech.com>
2024-07-13 18:52:47 -0500

    added error handling and commenting out what the function does

--- src/scraper.py
+++ src/scraper.py
@@ -1,15 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-#This 
+#This do the request to the url and the raise_for_status function returns if there're error or no and returns the content of the url in case the url is accessible
 def fetch_page_content(url):
-    response = requests.get(url)
-    status = response.raise_for_status()
-    result = ""
+    try:
+        response = requests.get(url)
+        status = response.raise_for_status()
+        result = ""
 
-    if status == None:
-        result = BeautifulSoup(response.text, 'html.parser')
-    else:
-        result = status
-    
-    return result
+        if status == None:
+            result = BeautifulSoup(response.text, 'html.parser')
+        else:
+            result = status
+        
+        return result
+    except Exception as error:
+        print(error)    
